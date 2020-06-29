bbaee7c920d948a893ffe5f93f9158c2efefd084
The Otterlord <64310361+TheOtterlord@users.noreply.github.com>
2020-06-29 10:49:38 +0100

    Remove else in parse_str_fail

--- src/main.rs
+++ src/main.rs
@@ -868,9 +868,6 @@ mod tests {
             // There is a problem with the parser
             // Output the result in an error (failing the test)
             panic!("{:?}", parsed_content);
-        } else {
-            // The code fails, which is what we want
-            return parse_str(r#""#);
         }
     }
 

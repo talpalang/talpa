23592609c1444352688e5e83ba1a0d2dce93f588
mjarkk <mkopenga@gmail.com>
2020-07-03 17:17:34 +0200

    Changed the namebuilder to a tuple

--- src/lib/statics.rs
+++ src/lib/statics.rs
@@ -4,21 +4,17 @@ pub static VALID_NAME_CHARS: &'static str =
   "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_";
 
 #[derive(Debug)]
-pub struct NameBuilder {
-  buff: Vec<u8>,
-}
+pub struct NameBuilder(Vec<u8>);
 
 impl NameBuilder {
   pub fn new() -> Self {
-    Self { buff: vec![] }
+    Self(vec![])
   }
   pub fn new_with_char(first_char: char) -> Self {
-    Self {
-      buff: vec![first_char as u8],
-    }
+    Self(vec![first_char as u8])
   }
   pub fn to_string<'a>(&self, p: &'a Parser) -> Result<String, ParsingError> {
-    match String::from_utf8(self.buff.clone()) {
+    match String::from_utf8(self.0.clone()) {
       Ok(parsed_string) => match parsed_string.get(0..1) {
         Some(v) if "1234567890".contains(v) => {
           p.error(ParsingErrorType::Custom("name cannot start with a number"))
@@ -29,10 +25,10 @@ impl NameBuilder {
     }
   }
   pub fn len(&self) -> usize {
-    self.buff.len()
+    self.0.len()
   }
   pub fn push(&mut self, value: char) {
-    self.buff.push(value as u8);
+    self.0.push(value as u8);
   }
 }
 
